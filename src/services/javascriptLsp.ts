5d3b5dac59cf702dbd89be4e7ed4c6c6955712e7
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-12-25 20:08:22 +0530

    using vscode lsp for our implementation now

--- src/services/javascriptLsp.ts
+++ src/services/javascriptLsp.ts
@@ -1,16 +1,20 @@
 import { ILspService } from "./lspService";
-
-export class JavascriptLSP implements ILspService{
+import { createConnection, ProposedFeatures, TextDocuments} from "vscode-languageserver/node";
+import { TextDocument } from 'vscode-languageserver-textdocument';
+export class JavascriptLsp implements ILspService {
+  private server: any;
+  private documents : TextDocuments<TextDocuments>;
   constructor(){
-
+    this.server = createConnection(ProposedFeatures.all)
+    this.documents = new TextDocuments(TextDocument)
   }
-  public start(): void {
-    
+
+  public start(): void { 
   }
   public listen(): void {
 
   }
   public translate(input: string): string {
-      return ""
+    return input
   }
-}
+} 
