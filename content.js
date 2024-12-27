060a557f1dbabe3d9d580ff2927d164f6024dbb4
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-12-27 22:03:12 +0530

    remove comments

--- content.js
+++ content.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   alert('button clicked yo')
   const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
   console.log(`tablll::`, tab)
+
   await chrome.scripting.executeScript({
     target: {tabId: tab.id},
     function: startTypingDetection
@@ -15,12 +16,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function startTypingDetection() {
   console.log('Detection started');
-  document.addEventListener('keydown', function(event) {
-    if (event.target.tagName === 'INPUT' || 
-        event.target.tagName === 'TEXTAREA' || 
-        event.target.isContentEditable) {
-      console.log('Typing detected in:', event.target.tagName);
-      console.log(`data:`, event.key)
-    }
+  document.addEventListener('keyup', function(event) {
+    console.log('Typing detected in:', event.target.tagName);
+    console.log(`hey` )
+    console.log(`data:`, event.key)
+    // }
   });
 }
